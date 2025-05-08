428e584ae4f2e2a6f37f813a68577d1f37439487
dcuevas5 <dcuevas5@ivytech.edu>
2025-05-08 17:15:38 -0400

    First commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
-def home(request):
-    context = {'nombre': 'David'}  
-    return render(request, 'myapp/home.html', context)
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'myapp/create_post.html', {'form': form})
